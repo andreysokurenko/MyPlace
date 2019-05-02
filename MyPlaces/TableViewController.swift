1f8bc5f3de8b7af18072c120fe1fec133ddb9505
andreysokurenko <andreysokurenko2@gmail.com>
2019-05-03 01:41:06 +0300

    Initial

---
+++ MyPlaces/TableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TableViewController.swift
+//  MyPlaces
+//
+//  Created by Andrey on 5/3/19.
+//  Copyright © 2019 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        
+
+        return cell
+    }
+    
+    
+}
