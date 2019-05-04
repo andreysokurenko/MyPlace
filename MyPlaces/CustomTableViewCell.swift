8139896e92ee9510510ac3d633bea73749cb83e5
andreysokurenko <andreysokurenko2@gmail.com>
2019-05-05 02:51:48 +0300

    CustomTableView

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Andrey on 5/5/19.
+//  Copyright © 2019 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
